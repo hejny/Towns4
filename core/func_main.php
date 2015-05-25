de1731fcb2a310927a4e2a445675cf18777bb230
Pavol Hejný <ph@towns.cz>
2015-05-25 13:17:13 +0200

    css js

--- core/func_main.php
+++ core/func_main.php
@@ -138,9 +138,11 @@ function rebase($url){
 	//$i=strpos($url,'/'.w.'/');$c=strlen(w);//
 	//$url=substr($url,0,$i).substr($url,$i+$c+1);
 
-    $cacheurl=url.cache;
-    $cacheurl=str_replace(w,'',$cacheurl);
-    $url=str_replace($cacheurl,$GLOBALS['inc']['cacheurl'],$url);
+    if(substr($url,-3)!=='.js' and substr($url,-4)!=='.css') {
+        $cacheurl = url . cache;
+        $cacheurl = str_replace(w, '', $cacheurl);
+        $url = str_replace($cacheurl, $GLOBALS['inc']['cacheurl'], $url);
+    }
 
 	return($url);
 }
