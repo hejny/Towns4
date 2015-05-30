803091d7931094a9c68f5858ff0deb01b6b247fc
Pavol Hejný <ph@towns.cz>
2015-05-30 16:50:58 +0200

    [Apps][PH] Storytext inteligent

--- app/api/internal/storytext.php
+++ app/api/internal/storytext.php
@@ -31,19 +31,18 @@ $result = $result['objects'];
 
 //----------------------------------------------------------------Příběhy - zobrazení
 if($result)
-foreach($result as $story){
+foreach($result as $story) {
 
 
-
-    $story['res'] = explode(':',$story['res'],2);//res u každého příběhu je ve formátu html:text příběhu, proto je potřeba html: useknout.
-    $story['res']=$story['res'][1];
+    $story['res'] = explode(':', $story['res'], 2);//res u každého příběhu je ve formátu html:text příběhu, proto je potřeba html: useknout.
+    $story['res'] = $story['res'][1];
 
     //echo($story['res']);
-    $story['res']='-'.$story['res'].'-';
+    $story['res'] = '-' . $story['res'] . '-';
 
 
-    foreach(array('img','iframe') as $tag) {
-        $story['res'] = explode('<'.$tag, $story['res']);
+    foreach (array('img', 'iframe') as $tag) {
+        $story['res'] = explode('<' . $tag, $story['res']);
         $i = 0;
         foreach ($story['res'] as &$tmp) {
             if ($i % 2) {
@@ -55,23 +54,25 @@ foreach($result as $story){
         $story['res'] = implode('', $story['res']);
     }
 
-    $story['res']=substr($story['res'],1,strlen($story['res'])-2);
+    $story['res'] = substr($story['res'], 1, strlen($story['res']) - 2);
 
-?>
+    if($story['res']){
+    ?>
             </p>
 
             <div style="text-align:center;font-weight: 600;">
             <div style="text-align:center;font-weight: 600;font-size: 1.41em;">
-            <?=$story['name']?>
+            <?= $story['name'] ?>
             </div>
-            <?=$_GET['noname']?'':$story['own_name']?>
+            <?= $_GET['noname'] ? '' : $story['own_name'] ?>
             </div>
 
-            <?=$story['res']?>
+            <?= $story['res'] ?>
 
             </p>
 
-<?php
+    <?php
+    }
 
 
 }
